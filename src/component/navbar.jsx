a1ef43ba3e0ca74bdd439b0b098ba2fae63e31a8
pratik0217 <jadhavpratik0208@gmail.com>
2025-11-06 13:24:13 +0530

    check the navbar working in responise desgin

---
+++ src/component/navbar.jsx
@@ -0,0 +1,80 @@
+import { NavLink } from "react-router";
+
+function navbar(props) {
+  const toggleMenu = () => {
+    document.querySelector("#toggle-navbar").classList.toggle("hidden");
+    console.log("clicked");
+  };
+
+//   const toggleMenuClose = () => {
+//     document.querySelector("#toggle-navbar").classList.toggle("hidden");
+//   };
+  return (
+    <div id="navbar-menu" className="flex justify-between items-center">
+      <div id="navbar-logo">
+        <a href="#" className="flex items-center gap-2">
+          <img
+            className="object-cover max-w-11 max-h-12"
+            src={props.navbarLogo}
+            alt="Navbar Logo"
+          />
+          <span className="text-lg font-medium">ToDesktop</span>
+        </a>
+      </div>
+
+      <div id="service-menu">
+        <nav className="hidden md:flex gap-10"> 
+          <NavLink id="nav-menulist" className="text-lg">Pricing</NavLink>
+          <NavLink id="nav-menulist" className="text-lg">Docs</NavLink>
+          <NavLink id="nav-menulist" className="text-lg">Changelog</NavLink>
+          <NavLink id="nav-menulist" className="text-lg">Blog</NavLink>
+          <NavLink id="nav-menulist" className="text-lg">Login</NavLink>
+        </nav>
+      </div>
+
+      <div
+        id="developer-toggle-button"
+        className="hidden md:flex items-center gap-2 border border-gray-300 rounded-lg px-6 py-2 hover:border-gray-700 "
+      >
+        <img src={props.developerToggle} />
+        <p className="text-lg">
+          Electron Developers <i className="fa-solid fa-arrow-right"></i>
+        </p>
+      </div>
+
+      <div id="menu-bar" onClick={toggleMenu} className="md:hidden flex items-center text-2xl">
+        <i className="fa-solid fa-bars"></i>
+      </div>
+
+      {/* Toggle Navbar  */}
+      <div id="toggle-navbar" className="fixed bg-white inset-0 md:hidden ">
+        <div id="navbar-menu" className="flex justify-between">
+          <div id="navbar-logo">
+            <a href="#" className="flex items-center gap-2">
+                <img className="object-cover max-w-11 max-h-12" src={props.navbarLogo} alt="Navbar Logo"/>
+                <span className="text-lg font-medium">ToDesktop</span>
+            </a>
+          </div>
+          <div id="menu-bar" onClick={toggleMenu} className="md:hidden flex items-center text-2xl"><i className="fa-solid fa-xmark"></i></div>
+        </div>
+        
+        <div id="toggle-menubar">
+            <nav className="flex flex-col items-start gap-7"> 
+                <NavLink id="nav-menulist" className="text-lg">Pricing</NavLink>
+                <NavLink id="nav-menulist" className="text-lg">Docs</NavLink>
+                <NavLink id="nav-menulist" className="text-lg">Changelog</NavLink>
+                <NavLink id="nav-menulist" className="text-lg">Blog</NavLink>
+                <NavLink id="nav-menulist" className="text-lg">Login</NavLink>
+            </nav>
+        </div>
+
+        <div id="toggle-menu-button" className="md:hidden flex justify-center items-center gap-2 border border-gray-500 rounded-lg max-w-60">
+        <img src={props.developerToggle} />
+        <p className="text-lg">Electron Developers</p>
+      </div>
+      </div>
+    </div>
+  );
+}
+
+export default navbar;
