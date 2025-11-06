c92b2ddd509085bbe01ef2cde8974794db2f5d27
pratik0217 <jadhavpratik0208@gmail.com>
2025-11-06 22:55:45 +0530

    check the response design for mobile device

--- src/component/mainSection.jsx
+++ src/component/mainSection.jsx
@@ -32,7 +32,7 @@ export default function mainSection(props) {
     </div>
 
     {/* Mobile device Response design */}
-    <div id="hero-section-mobile" className="md:hidden flex flex-col items-center gap-6">
+    <div id="hero-section-mobile" className="md:hidden flex flex-col items-center gap-8">
         <div id="version-box-mobile" className="flex items-center gap-2 border border-amber-500 rounded-lg bg-orange-50 font-medium">
            <span className="inline-block w-3 h-3 border border-black rounded-full bg-amber-400"></span>
            <span className="text-amber-600">v0.21.1: </span>
@@ -48,11 +48,11 @@ export default function mainSection(props) {
 
         <h1 className="text-start text-5xl font-bold text-zinc-800 w-[360px]">Web app to Desktop app in minutes</h1>
         
-        <p className="text-start w-[325px]">Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
+        <p className="text-xl text-start w-[360px]">Take your web app codebase and transform it into a cross platform desktop app with native functionality.</p>
 
-        <div id="hero-sect-btn" className="flex flex-col gap-3">
-            <button className="rounded-lg w-[325px]">Download now</button>
-            <button className="border border-gray-500 rounded-lg">Read more</button>
+        <div id="hero-sect-mobile-btn" className="flex flex-col gap-5">
+            <button className="rounded-lg w-[360px] font-medium">Download now</button>
+            <button className="border border-gray-500 rounded-lg w-[360px] font-medium">Read more</button>
         </div>
 
         <div id="app-section" className="flex gap-5">
